00cfd890ad2a5f933d1780d4c67f627ba56e5263
Jack H <jackhe678@gmail.com>
2017-11-21 10:31:52 -0500

    todos php

---
+++ PhpTodos/class.database.php
@@ -0,0 +1,12 @@
+<?php
+
+    class MongoDBConnection
+    {
+        protected $db_connection;
+        public $db_name = 'todo';
+        public $db_user = 'root';
+        public $db_pass = '';
+        public $db_host = 'localhost:27017';
+
+       
+    }
